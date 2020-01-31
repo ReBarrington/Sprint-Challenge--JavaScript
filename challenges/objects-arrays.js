85dc85957dd6ea25938161962e9f1709f6b4e753
Reagan Barrington <reagan@barringtonfamily.net>
2020-01-31 13:00:47 -0500

    stretch goal on objects-arrays completed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -140,11 +140,7 @@ The zoos want to display both the scientific name and the animal name in front o
 */
 
 const displayNames = [];
-
-zooAnimals.forEach(function(item) {
-    displayNames.push(`Name: ${item.animal_name}, Scientific: ${item.scientific_name}`);
-  })
-
+zooAnimals.forEach(item => displayNames.push(`Name: ${item.animal_name}, Scientific: ${item.scientific_name}`))
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -153,15 +149,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-
 const lowCaseAnimalNames = [];
-
-zooAnimals.map(function(item){
-  lowCaseAnimalNames.push(item.animal_name.toLowerCase())
-})
-
+zooAnimals.map(item => lowCaseAnimalNames.push(item.animal_name.toLowerCase()))
 console.log(lowCaseAnimalNames);
-
 
 /* Request 3: .filter() 
 
@@ -169,27 +159,19 @@ The zoos are concerned about animals with a lower population count. Using filter
 
 */
 
-const lowPopulationAnimals = zooAnimals.filter(function(animal){
-  return animal.population < 5;
-})
-
-console.log(lowPopulationAnimals);
+const lowPopulationAnimals = zooAnimals.filter(animal => animal.population < 5);
+console.log(lowPopulationAnimals)
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = zooAnimals.reduce(function(accumulator, item) {
-  return accumulator + item.population;
-}, 0); 
+const populationTotal = zooAnimals.reduce((accumulator, item) => accumulator + item.population,0); 
 console.log(populationTotal);
-
-
 
 /*
 
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
-
 */
 
